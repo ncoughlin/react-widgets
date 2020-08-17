9c1e943fe86e90f0964a22e6614bbac42d573c7f
Your Name <coughlin.nicholas@gmail.com>
2020-08-17 09:58:23 -0700

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import Accordion from "./components/Accordion";
+
+const items = [
+    {
+        title: "title1",
+        content: 'content1'
+    },
+    {
+        title: "title2",
+        content: 'content2'
+    },
+    {
+        title: "title3",
+        content: 'content3'
+    }
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/Accordion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Accordion = () => {
+    return (
+        <div>
+            Accordion
+        </div>
+    );
+}
+
+export default Accordion;
