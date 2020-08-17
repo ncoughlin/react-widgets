9be7db88a820c19c700a6e24c9cf50e0d64cd41f
Your Name <coughlin.nicholas@gmail.com>
2020-08-17 13:40:57 -0700

    items are toggling

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import React from "react";
 import Accordion from "./components/Accordion";
 
 const items = [
-    {
-        title: "title1",
-        content: 'content1'
-    },
-    {
-        title: "title2",
-        content: 'content2'
-    },
-    {
-        title: "title3",
-        content: 'content3'
-    }
+  {
+    title: "Excepteur sint occaecat",
+    content:
+      "Morbi tristique senectus et netus et malesuada. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Morbi tincidunt ornare massa eget egestas purus viverra. Ut tristique et egestas quis ipsum. Nunc faucibus a pellentesque sit amet porttitor eget. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. ",
+  },
+  {
+    title: "Duis aute irure dolor",
+    content:
+      "Augue interdum velit euismod in pellentesque massa placerat. Id aliquet lectus proin nibh nisl condimentum id venenatis. Non enim praesent elementum facilisis leo vel fringilla. Semper eget duis at tellus at urna condimentum mattis pellentesque. ",
+  },
+  {
+    title: "Ut enim ad minim",
+    content:
+      "Lacus luctus accumsan tortor posuere ac ut consequat semper. Suscipit adipiscing bibendum est ultricies integer quis auctor. Nunc sed velit dignissim sodales ut. Vestibulum morbi blandit cursus risus at ultrices mi. Netus et malesuada fames ac turpis egestas integer.",
+  },
 ];
 
 export default () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
+  const [activeTab, setActiveTab] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveTab(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    //  if current index is same as active tab, add class "active"
+    const activeStatus = index === activeTab ? 'active' : '';
+
     return (
-        <div>
-            Accordion
+      <React.Fragment key={item.title}>
+        <div className={`title ${activeStatus}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
         </div>
+        <div className={`content ${activeStatus}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
     );
-}
+  });
+
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+    </div>
+  );
+};
 
 export default Accordion;
