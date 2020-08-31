7178276614c0f814bd1d770cb6ec18c04d078853
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-08-31 04:35:24 -0700

    filtered current selection from render list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Dropdown = ({ dropdownOptions, selected, onSelectedChange }) => {
-  const mappedDropdownOptions = dropdownOptions.map((option) => {
+  const renderedOptions = dropdownOptions.map((option) => {
+    // if current selection is equal to option do not generate div
+    if (option.value === selected.value){
+      return null;
+    }
+
     return (
       <div 
         key={option.value} 
@@ -21,7 +26,7 @@ const Dropdown = ({ dropdownOptions, selected, onSelectedChange }) => {
         <div className="ui selection dropdown visible active">
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{mappedDropdownOptions}</div>
+          <div className="menu visible transition">{renderedOptions}</div>
         </div>
       </div>
     </div>
