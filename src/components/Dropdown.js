7752837e8b84921d456ba897775cb87509f0537b
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-08-31 05:14:00 -0700

    visibility is now toggled on click

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ dropdownOptions, selected, onSelectedChange }) => {
+  // state to manage toggle visibility
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = dropdownOptions.map((option) => {
     // if current selection is equal to option do not generate div
-    if (option.value === selected.value){
+    if (option.value === selected.value) {
       return null;
     }
 
     return (
-      <div 
-        key={option.value} 
+      <div
+        key={option.value}
         className="item"
         // on click change selection to current option
         onClick={() => onSelectedChange(option)}
@@ -23,10 +26,20 @@ const Dropdown = ({ dropdownOptions, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          // on click set value of open to opposite of current value
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div
+            // on click set value of open to opposite of current value
+            onClick={() => setOpen(!open)}
+            className={`menu ${open ? "visible transition" : ""}`}
+          >
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
