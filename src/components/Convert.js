02702261e78fb3c6a7aea68e40e8ce0a9e20aa1a
Nicholas Coughlin <coughlin.nicholas@gmail.com>
2020-09-04 13:27:12 -0700

    translation widget is working with debounced search term

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,14 +1,52 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 // de-structure language and text props
 const Convert = ({ language, text }) => {
-  useEffect(() => {
-    console.log("language or text has changed");
-  }, [language, text]);
+  const [results, setResults] = useState([]);
+  const [debouncedText, setDebouncedText] = useState(text);
 
-  return <div>
-      <p>{text}</p>
-  </div>;
+  // de-bouncing the search term
+  // runs every time the text changes  
+  useEffect(() => {
+      const timerId = setTimeout(() => {
+          setDebouncedText(text);
+      }, 500);
+
+      return () => {
+          clearTimeout(timerId);
+      };
+  }, [text]);
+
+  // runs every time language or text updates
+  useEffect(() => {
+    const translate = async () => {
+      const translation = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+
+      setResults(translation.data.data.translations[0].translatedText);
+
+      
+    };
+
+    translate();
+
+  }, [language, debouncedText]);
+
+  return (
+    <div>
+      <p>{results}</p>
+    </div>
+  );
 };
 
 export default Convert;
